ad4b0d84943af28a740b65f40bb9ada983d7e13f
Curtis Bateson <curtislbateson@gmail.com>
2017-11-03 01:05:41 +0000

    Finished all basic requirements

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const uuid = require('uuid/v4');
 
+const colors = ['#639', '#ff6347', '#DE1E7E', '#F00'];
+
 // Set the port to 3001
 const PORT = 3001;
 
@@ -20,11 +22,37 @@ const wss = new SocketServer({ server });
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
-  console.log('Client connected');
+  // console.log('Client connected');
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === ws.OPEN) {
+      client.send(JSON.stringify({
+        // id: uuid(),
+        num: wss.clients.size,
+        type: 'userNumUpdate'
+      }));
+    }
+  });
+
+  ws.color = colors[Math.floor((Math.random() * 4))];
+  ws.send(JSON.stringify({
+    color: ws.color,
+    type: 'userColor'
+  }));
 
   ws.on('message', (message) => {
     let messageObj = JSON.parse(message);
     messageObj.id = uuid();
+    switch (messageObj.type) {
+      case 'postMessage':
+      messageObj.type = 'incomingMessage';
+      break;
+      case 'postNotification':
+      messageObj.type = 'incomingNotification';
+      break;
+      default:
+      messageObj.type = 'unknown';
+      break;
+    }
     let messageStr = JSON.stringify(messageObj);
     wss.clients.forEach(function each(client) {
         if (client.readyState === ws.OPEN) {
@@ -34,5 +62,5 @@ wss.on('connection', (ws) => {
   });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  // ws.on('close', () => console.log('Client disconnected'));
 });
